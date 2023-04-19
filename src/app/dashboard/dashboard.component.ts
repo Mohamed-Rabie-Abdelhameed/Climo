c17829e2df6443d640ffbf4735354161e430e864
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2023-04-19 07:10:34 +0200

    finish today's higlights panel

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,8 +9,11 @@ import { Weather } from '../models/weather';
 })
 export class DashboardComponent implements OnInit {
   constructor(private API: WeatherService) {}
+
   @ViewChild('searchInput') searchInput!: ElementRef;
+
   loc: string = 'New York';
+
   weatherData: Weather = {
     time: '26262',
     temperature: 0,
@@ -28,14 +31,98 @@ export class DashboardComponent implements OnInit {
     animate: true,
   };
 
-  forecastData!: Weather[];
+  forecastData: Weather[] = [
+    {
+      time: '26262',
+      temperature: 0,
+      weatherCode: 1000,
+      cloudCover: 0,
+      weatherCondition: 'Clear, Sunny',
+      windSpeed: 0,
+      windDirection: 0,
+      humidity: 0,
+      uvIndex: 0,
+      visibility: 0,
+      precipitationProbability: 0,
+      temperatureApparent: 0,
+      name: 'undefined',
+      animate: true,
+    },
+    {
+      time: '26262',
+      temperature: 0,
+      weatherCode: 1000,
+      cloudCover: 0,
+      weatherCondition: 'Clear, Sunny',
+      windSpeed: 0,
+      windDirection: 0,
+      humidity: 0,
+      uvIndex: 0,
+      visibility: 0,
+      precipitationProbability: 0,
+      temperatureApparent: 0,
+      name: 'undefined',
+      animate: true,
+    },
+    {
+      time: '26262',
+      temperature: 0,
+      weatherCode: 1000,
+      cloudCover: 0,
+      weatherCondition: 'Clear, Sunny',
+      windSpeed: 0,
+      windDirection: 0,
+      humidity: 0,
+      uvIndex: 0,
+      visibility: 0,
+      precipitationProbability: 0,
+      temperatureApparent: 0,
+      name: 'undefined',
+      animate: true,
+    },
+    {
+      time: '26262',
+      temperature: 0,
+      weatherCode: 1000,
+      cloudCover: 0,
+      weatherCondition: 'Clear, Sunny',
+      windSpeed: 0,
+      windDirection: 0,
+      humidity: 0,
+      uvIndex: 0,
+      visibility: 0,
+      precipitationProbability: 0,
+      temperatureApparent: 0,
+      name: 'undefined',
+      animate: true,
+    },
+    {
+      time: '26262',
+      temperature: 0,
+      weatherCode: 1000,
+      cloudCover: 0,
+      weatherCondition: 'Clear, Sunny',
+      windSpeed: 0,
+      windDirection: 0,
+      humidity: 0,
+      uvIndex: 0,
+      visibility: 0,
+      precipitationProbability: 0,
+      temperatureApparent: 0,
+      name: 'undefined',
+      animate: true,
+    },
+  ];
+
+  isSmall: boolean = false;
 
   async ngOnInit() {
+    this.isSmall = this.isSmallScreen;
     this.loc = await this.API.getUserLocation();
     console.log(this.loc);
-    // this.forecastData = await this.API.getForecast(this.loc);
+    this.forecastData = await this.API.getForecast(this.loc);
     console.log(this.forecastData);
-    // this.weatherData = await this.API.getWeather(await this.loc);
+    this.weatherData = await this.API.getWeather(await this.loc);
   }
 
   formatDateTime(dateTimeString: string): string {
@@ -58,10 +145,20 @@ export class DashboardComponent implements OnInit {
   }
 
   formatLocation(location: string): string {
-    const parts = location.split(', ');
-    const city = parts[0];
-    const country = parts[parts.length - 1];
-    return `${city}, ${country}`;
+    try {
+      const parts = location.split(', ');
+      const city = parts[0];
+      const country = parts[parts.length - 1];
+      return `${city}, ${country}`;
+    } catch {
+      return location;
+    }
+  }
+
+  getWindDirection(degrees: number): string {
+    const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    const index = Math.round(degrees / 45) % 8;
+    return directions[index];
   }
 
   getDayOfWeek(dateTimeString: string): string {
@@ -87,5 +184,9 @@ export class DashboardComponent implements OnInit {
     this.weatherData.animate = true;
     console.log(this.weatherData);
     this.forecastData = await this.API.getForecast(location);
+  }
+
+  get isSmallScreen(): boolean {
+    return window.innerWidth < 768;
   }
 }
